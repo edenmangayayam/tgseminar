7999753a5ec276492205e2f5db5821babb10f51f
Eden <eden.mangayayam@rococoglobaltech.com>
2013-06-14 14:13:27 +0900

    Testing if entity does not exist

--- src/main/java/tgseminar/controller/UpdateController.java
+++ src/main/java/tgseminar/controller/UpdateController.java
@@ -2,6 +2,11 @@ package tgseminar.controller;
 
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.datastore.Datastore;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
 
 public class UpdateController extends Controller {
 
@@ -24,12 +29,28 @@ public class UpdateController extends Controller {
 			return null;
 		}
 		
-		if (title == null || "".equals(idString)){
+		if (title == null || "".equals(title)){
 			response.setStatus(400);
 			return null;
 		}
 		
+		Key key = Datastore.createKey("ToDo", id);
+//		Entity entity = Datastore.getOrNull(key);
+//		
+//		if(entity == null){
+//			response.setStatus(400);
+//			return null;
+//			
+//		}
 
+		Entity entity;
+		try {
+			entity = Datastore.get(key);
+		} catch (Exception e) {
+			response.setStatus(400);
+			return null;
+		}
+		
 		return null;
 	}
 
--- src/test/java/tgseminar/controller/UpdateControllerTest.java
+++ src/test/java/tgseminar/controller/UpdateControllerTest.java
@@ -1,13 +1,18 @@
 package tgseminar.controller;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.datastore.Datastore;
 import org.slim3.tester.ControllerTestCase;
+
+import com.google.appengine.api.datastore.Entity;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -29,7 +34,7 @@ public class UpdateControllerTest extends ControllerTestCase {
 	}
 
 	@Test
-	public void idIsNotNumeric() throws NullPointerException, 
+	public void respond400IfIdIsNotNumeric() throws NullPointerException, 
 		IllegalArgumentException, IOException, ServletException{
 		
 		tester.param("id", "hai");
@@ -43,7 +48,7 @@ public class UpdateControllerTest extends ControllerTestCase {
 	}
 
 	@Test
-	public void titleIsNotSpecified() throws NullPointerException, 
+	public void respond400IfTitleIsNotSpecified() throws NullPointerException, 
 		IllegalArgumentException, IOException, ServletException{
 		
 		tester.param("id", "1");
@@ -56,4 +61,39 @@ public class UpdateControllerTest extends ControllerTestCase {
 		
 	}
 
+	@Test
+	public void respond400IfEntityDoesNotExist() throws NullPointerException, 
+		IllegalArgumentException, IOException, ServletException{
+		
+		tester.param("id", "3");
+		tester.param("title", "To-Do #3 modified");
+		tester.start("/Update");
+		
+		//assert response from server is 400
+		
+		assertThat(tester.response.getStatus(), is(400));
+		
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		Entity entity1 = new Entity(Datastore.createKey("Todo", 1));
+		entity1.setProperty("title", "To-Do #1");
+		entity1.setProperty("createdBy", "loginuser@example.com");
+		entity1.setProperty("createdAt", new Date());
+
+		Entity entity2 = new Entity(Datastore.createKey("Todo", 1));
+		entity2.setProperty("title", "To-Do #1");
+		entity2.setProperty("createdBy", "loginuser@example.com");
+		entity2.setProperty("createdAt", new Date());
+		
+		Datastore.put(entity1, entity2);
+		
+		
+	}
+
+	
+	
+	
 }
